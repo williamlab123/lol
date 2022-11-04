70df2f643205ad853c0626060c9cac638cb7f63f
William Pedro <54008790+williamlab123@users.noreply.github.com>
2022-11-04 17:18:10 -0300

    Add files via upload

---
+++ lol/lol/index.js
@@ -0,0 +1,20 @@
+function chat()
+{
+    var btn_send = document.getElementById('post')
+
+    if(btn_send == true)
+    {
+    
+        alert('certo')
+
+        
+    }
+
+    else
+    {
+        alert('errado')
+    }
+    
+        
+    
+}
